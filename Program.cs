970f56e0f402d1a3574da4899625086238f5d603
Alex Brown <alex@cohoda.com>
2024-07-01 15:51:00 +0100

    remove stuff that didn't work properly.

--- Program.cs
+++ Program.cs
@@ -4,16 +4,11 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Serilog;
-using Serilog.Events;
 using SerilogAppInsightsAzureFunctions.Logging;
 using SerilogAppInsightsAzureFunctions.Services;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults()
-    .ConfigureAppConfiguration(config =>
-    {
-        config.AddUserSecrets<Program>(optional: true, reloadOnChange: false);
-    })
     .ConfigureServices(services =>
     {
         services.AddApplicationInsightsTelemetryWorkerService();
@@ -23,13 +18,6 @@ var host = new HostBuilder()
     .UseSerilog((context, services, loggerConfiguration) =>
     {
         loggerConfiguration
-            .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
-            .MinimumLevel.Override("Worker", LogEventLevel.Warning)
-            .MinimumLevel.Override("Host.*", LogEventLevel.Warning)
-            .MinimumLevel.Override("System", LogEventLevel.Error)
-            .MinimumLevel.Override("Function.*", LogEventLevel.Error)
-            .MinimumLevel.Override("Azure.Storage.Blobs", LogEventLevel.Error)
-            .MinimumLevel.Override("Azure.Core", LogEventLevel.Error)
             .Enrich.FromLogContext()
             .Enrich.WithOperationId()
             .WriteTo.ApplicationInsights
